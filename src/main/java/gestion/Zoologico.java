559ade4a06025b2f0de4e1f1354111b4aed59a2c
RicardoLopera <rloperav@unal.edu.co>
2023-09-15 05:49:01 -0500

    :+1:

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -39,5 +39,11 @@ public class Zoologico {
 	public void setUbicacion(String ubicacion) {
 		this.ubicacion = ubicacion;
 	}
-	
+	public ArrayList<Zona> getZona() {
+		return zonas;
+	}
+	public void setZonas(ArrayList<Zona> zonas) {
+		this.zonas = zonas;
+	}
+
 }
